b9f4c5748dc923b7f74502d0e61b2dce102bb2c8
SergeyChadik <bunubon@yandex.ru>
2021-07-09 23:50:15 +0200

    57_preloader

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,60 +1,38 @@
 import React from 'react';
-import styles from './users.module.css';
-import * as axios from 'axios';
-import userPhoto from '../../images/user-profile.png'
+import styles from "./users.module.css";
+import userPhoto from "../../images/user-profile.png";
 
-let Users = (props) => {
-    let getUsers = () =>
-    {
-        if (props.users.length === 0) {
+export let Users = (props) => {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        });}
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-
-        /* props.setUsers(
-             [
-                 {
-                     id: 1,
-                     photoUrl: 'https://www.meme-arsenal.com/memes/4e4e3eca35797638c567d55e62dc392e.jpg',
-                     followed: false,
-                     fullName: 'Sergey',
-                     status: 'lok at me',
-                     location: {city: "Minsk", country: 'Belarus'}
-                 },
-                 {
-                     id: 2,
-                     photoUrl: 'https://www.meme-arsenal.com/memes/4e4e3eca35797638c567d55e62dc392e.jpg',
-                     followed: true,
-                     fullName: 'Valery',
-                     status: 'lok at me',
-                     location: {city: "Mans", country: 'Russia'}
-                 },
-                 {
-                     id: 3,
-                     photoUrl: 'https://www.meme-arsenal.com/memes/4e4e3eca35797638c567d55e62dc392e.jpg',
-                     followed: false,
-                     fullName: 'Artem',
-                     status: 'lok at me',
-                     location: {city: "Min", country: 'ukr'}
-                 },
-             ]
-         )
-     }*/
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++){
+        pages.push(i);
     }
+
     return <div>
-        <button onClick={getUsers}>Get Users</button>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage}
+                             onClick={(e) => {props.onPageChanged(p);
+                             }}>{p}</span> })}
+
+        </div>
         {
-            props.users.map(u => <div key={u.id}>
+             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={ () => { props.unfollow(u.id)}}> unFollow</button>
-                            : <button onClick={ () => { props.follow(u.id)}} >Follow</button>}
+                            ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}> unFollow</button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
 
                     </div>
                 </span>
@@ -73,9 +51,3 @@ let Users = (props) => {
         }
     </div>
 }
-
-
-
-export default Users;
-
-
